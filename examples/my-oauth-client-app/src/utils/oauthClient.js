6e066d8bffeea075a1d66c766481f0a2daf26ad8
maniSHarma7575 <hacker.sharma7575@gmail.com>
2025-03-06 23:03:35 +0530

    [FIX] Client secret and client id are not passed

--- examples/my-oauth-client-app/src/utils/oauthClient.js
+++ examples/my-oauth-client-app/src/utils/oauthClient.js
@@ -1,8 +1,9 @@
-import { OAuthXClient } from "@manisharma7575/oauthx";
+import { OAuthXClient } from "oauthx";
 
 export const oauthClient = new OAuthXClient({
   server: import.meta.env.VITE_AUTH_SERVER,
   client_id: import.meta.env.VITE_CLIENT_ID,
+  client_secret: import.meta.env.VITE_CLIENT_SECRET,
   redirect_uri: import.meta.env.VITE_REDIRECT_URI,
   tokenEndpoint: import.meta.env.VITE_TOKEN_ENDPOINT,
 });
