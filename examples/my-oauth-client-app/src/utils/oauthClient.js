9de629bfed2c29305db5a12093d3943abc9a1ec3
maniSHarma7575 <hacker.sharma7575@gmail.com>
2025-03-07 12:50:15 +0530

    For client side we don't need to pass the client secret

--- examples/my-oauth-client-app/src/utils/oauthClient.js
+++ examples/my-oauth-client-app/src/utils/oauthClient.js
@@ -3,7 +3,6 @@ import { OAuthXClient } from "@manisharma7575/oauthx";
 export const oauthClient = new OAuthXClient({
   server: import.meta.env.VITE_AUTH_SERVER,
   client_id: import.meta.env.VITE_CLIENT_ID,
-  client_secret: import.meta.env.VITE_CLIENT_SECRET,
   redirect_uri: import.meta.env.VITE_REDIRECT_URI,
   token_endpoint: import.meta.env.VITE_TOKEN_ENDPOINT,
 });
