321108348a4aa91c9a1ffbb7116e67ed93a6e2b9
maniSHarma7575 <hacker.sharma7575@gmail.com>
2025-03-07 09:01:31 +0530

    Release 1.0.2

--- examples/my-oauth-client-app/src/utils/oauthClient.js
+++ examples/my-oauth-client-app/src/utils/oauthClient.js
@@ -1,11 +1,11 @@
-import { OAuthXClient } from "@manisharma7575/oauthx";
+import { OAuthXClient } from "oauthx";
 
 export const oauthClient = new OAuthXClient({
   server: import.meta.env.VITE_AUTH_SERVER,
   client_id: import.meta.env.VITE_CLIENT_ID,
   client_secret: import.meta.env.VITE_CLIENT_SECRET,
   redirect_uri: import.meta.env.VITE_REDIRECT_URI,
-  tokenEndpoint: import.meta.env.VITE_TOKEN_ENDPOINT,
+  token_endpoint: import.meta.env.VITE_TOKEN_ENDPOINT,
 });
 
 export const AUTH_SERVER = import.meta.env.VITE_AUTH_SERVER;
